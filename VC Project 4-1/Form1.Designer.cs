20ae957feba17fe857685ce46af6bbb00cea814a
jlarson497 <kf2617gi@go.minneapolis.edu>
2015-10-13 12:40:22 -0500

    Added validation and methods for calculation

---
+++ VC Project 4-1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace VC_Project_4_1
+{
+    partial class frmAnP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.txtPerimeter = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Length: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Width: ";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(24, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Area: ";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(2, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Perimeter: ";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(65, 9);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 4;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(65, 29);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 5;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(65, 50);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.ReadOnly = true;
+            this.txtArea.Size = new System.Drawing.Size(100, 20);
+            this.txtArea.TabIndex = 6;
+            this.txtArea.TabStop = false;
+            // 
+            // txtPerimeter
+            // 
+            this.txtPerimeter.Location = new System.Drawing.Point(65, 70);
+            this.txtPerimeter.Name = "txtPerimeter";
+            this.txtPerimeter.ReadOnly = true;
+            this.txtPerimeter.Size = new System.Drawing.Size(100, 20);
+            this.txtPerimeter.TabIndex = 7;
+            this.txtPerimeter.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(16, 118);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(132, 118);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmAnP
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(226, 165);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtPerimeter);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmAnP";
+            this.Text = "Area and Perimeter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.TextBox txtPerimeter;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ VC Project 4-1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VC_Project_4_1
+{
+    /*************************************************************************
+     * This is a simple program that takes user inputs of lenth and, of a
+     * square or rectangle, and calculates the area and perimeter of the 
+     * shape.  And that's it.  It doesn't not do any data validation, so 
+     * the user must enter numeric values.  It is only slightly easier
+     * than doing the math with pen and paper, and depending on your
+     * computer's load times and your personal math skillz, it may take
+     * even more time.
+     * 
+     * Produced by Joseph Larson
+     *************************************************************************/
+    public partial class frmAnP : Form
+    {
+        public frmAnP()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            //validate form
+            if (!IsValidForm())
+            {
+               return;
+            }
+            //Take in the user inputs and convert to decimal
+            //Then create 2 more variables to hold the answers
+            //Do some math and display in the read only textboxes
+            decimal width = Convert.ToDecimal(txtWidth.Text);
+            decimal length = Convert.ToDecimal(txtLength.Text);
+            decimal area = CalculateArea(length, width);
+            decimal perimeter = CalculatePerimeter(length, width);
+
+            txtArea.Text = Convert.ToString(area);
+            txtPerimeter.Text = Convert.ToString(perimeter);
+            txtLength.Focus();
+
+            //the last line brings the focus back to the first box in
+            //case the user wants to have some more fun
+
+        }
+        public decimal CalculateArea(decimal length, decimal width)
+        {
+            decimal area = length * width;
+            return area;
+        }
+
+        public decimal CalculatePerimeter(decimal length, decimal width)
+        {
+            decimal perimeter = (2 * length) + (2 * width);
+            return perimeter;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Event handler for the exit button
+            this.Close();
+        }
+
+        public bool IsDecimal(TextBox textbox, string name)
+        {
+            decimal number = 0;
+            if (decimal.TryParse(textbox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a number.", "Entry Error");
+                textbox.Clear();
+                textbox.Focus();
+                return false;
+            }
+
+        }
+
+        public bool IsPresent(TextBox textbox, string name)
+        {
+            if (textbox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textbox.Focus();
+                return false;
+            }
+            else 
+            {
+                return true;
+            }
+
+        }
+
+        public bool IsValidForm()
+        {
+            return
+                IsPresent(txtLength, "Length") && IsDecimal(txtLength, "Length") 
+                && IsPresent(txtWidth, "Width") && IsDecimal(txtWidth, "Width");
+        }
+    }
+}
