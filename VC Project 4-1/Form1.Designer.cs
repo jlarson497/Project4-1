488b37ada3ddab6403a44025ad6055e3d5cc68f2
jlarson497 <kf2617gi@go.minneapolis.edu>
2015-10-13 20:01:07 -0500

    Added hotkeys to labels

--- VC Project 4-1/Form1.Designer.cs
+++ VC Project 4-1/Form1.Designer.cs
@@ -48,7 +48,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Length: ";
+            this.label1.Text = "&Length: ";
             // 
             // label2
             // 
@@ -57,7 +57,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Width: ";
+            this.label2.Text = "&Width: ";
             // 
             // label3
             // 
